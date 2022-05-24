c97d5620c7509b37423b8314f15dab5e4110ce94
Jonathan Ferreira <big.jhou2@gmail.com>
2022-05-24 00:33:10 -0300

    remove bdstring config

--- config/config.ts
+++ config/config.ts
@@ -2,7 +2,6 @@ const env = process.env.NODE_ENV || 'dev';
 import 'dotenv/config'
 
 const config  = () => {
-    //return {bdString: "mongodb+srv://deoz_user:BBuebn7jYjOraTv1@cluster0.7lcvj.mongodb.net/positivoNegativo?retryWrites=true&w=majority", jwtKey: "1q2w3e4r"}
     return {bdString: process.env.DATABASE_URL, jwtKey: process.env.JWT_SECRET}
 }
  export {config};
